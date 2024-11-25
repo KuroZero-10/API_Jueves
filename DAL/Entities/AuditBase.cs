a16ce22d414a5c40b1c0e18880187b2106617d70
Dilson Garcia <chombanito.1999@gmail.com>
2024-11-25 17:51:05 -0500

    Set up DB configuration and creation

---
+++ DAL/Entities/AuditBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingAPI_Jueves_2024II.DAL.Entities
+{
+    public class AuditBase
+    {
+        [Key] //Primary key
+        [Required] //Campo obligatorio
+        public virtual Guid Id { get; set; } //Este será el PK de todas las tablas
+        public virtual DateTime? CreatedDate { get; set; }//Fecha de creacion de cada registro
+        public virtual DateTime? ModifiedDate { get; set; }//Fechas del cambio de cada registro
+    }
+}
---
+++ DAL/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingAPI_Jueves_2024II.DAL.Entities
+{
+    public class Country:AuditBase
+    {
+        [Display(Name="País")] //Para identificar el nombre mas facil
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(50,ErrorMessage ="El campo {0} debe tener maximo {1} caracteres")]
+        public string Name { get; set; }
+
+    }
+}
