167bf548363f47275920b5ff8983e248634e3922
Dilson Garcia <chombanito.1999@gmail.com>
2024-11-25 19:24:18 -0500

    ICountryService cretated

---
+++ Domain/Interfaces/ICountryService.cs
@@ -0,0 +1,16 @@
+﻿ using ShoppingAPI_Jueves_2024II.DAL.Entities;
+
+namespace ShoppingAPI_Jueves_2024II.Domain.Interfaces
+{
+    public interface ICountryService
+    {
+        //get by id, get all, create, update, delete; son las operaciones principales, pueden haber mas...
+        // Collections: IList  lista completa, ICollection listas no ordenables, IEnumerable lista estática, IQuerable lista lógica
+        Task<IEnumerable<Country>> GetCountriesAsync();
+
+        Task<Country> CreateCountryAsync(Country country);
+        Task<Country> GetCountryByIdAsync(Guid id);
+        Task<Country> EditCountryAsync(Country country);
+        Task<Country> DeleteCountryAsync(Guid id);
+    }
+}
